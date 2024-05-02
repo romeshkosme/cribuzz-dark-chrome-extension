c647f2c8a3d218f5bc9b42febf72f8a2a45eec7d
Romesh Kosme <romesh.30@gmail.com>
2024-05-02 22:59:45 +0530

    feat: series schedule result page into dark mode

---
+++ scripts/mutate-schedule-result.js
@@ -0,0 +1,80 @@
+import {SUBSCRIPTION_LIST} from "./content.js";
+import {CONTAINER_BG, DARK_BLOCK} from "./utils.js";
+import {match_nav_head, match_nav_links} from "./match-content.js";
+
+export default function mutate_schedule_result() {
+  try {
+    match_nav_head();
+    match_nav_links();
+    mutate_bg();
+    mutate_schedule_table_header();
+    mutate_match_block();
+    mutate_match_count_length();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function mutate_bg() {
+  try {
+    const NODE = document.querySelector("#page-wrapper > div.cb-bg-white.cb-col-100.cb-col.cb-hm-rght.cb-series-filters");
+    if (!NODE) return;
+
+    NODE.style.backgroundColor = CONTAINER_BG;
+    NODE.style.color = "#fff";
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function mutate_schedule_table_header() {
+  try {
+    const NODE = document.querySelector("#series-matches > div.cb-col-100.cb-col.cb-srs-gray-strip");
+    if (!NODE) return;
+
+    NODE.style.backgroundColor = DARK_BLOCK;
+    NODE.style.color = "#fff";
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+function mutate_match_block(){
+  try {
+    const NODE = document.querySelectorAll(".cb-srs-mtchs-tm");
+    if (!NODE) return;
+console.log(NODE)
+    for (const elem of NODE) {
+      const match_link = elem.querySelector("a.text-hvr-underline")
+      if (match_link) match_link.style.color = "#fff";
+
+      const match_location = elem.querySelector("div.text-gray");
+      if (match_location) match_location.style.color = "lightgray";
+
+      const match_complete = elem.querySelector("a.cb-text-complete");
+      if (match_complete) match_complete.style.color = "#7ebdc3"
+
+      const match_preview = elem.querySelector("a.cb-text-preview");
+      if (match_preview) match_preview.style.color = "#f78764";
+
+      const match_upcoming = elem.querySelector("a.cb-text-upcoming");
+      if (match_upcoming) match_upcoming.style.color = "#f78764";
+
+      const match_inprogress = elem.querySelector("a.cb-text-inprogress");
+      if (match_inprogress) match_inprogress.style.color = "#e63946";
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+function mutate_match_count_length() {
+  try {
+    const NODE = document.querySelector("#page-wrapper > div.cb-col-100.cb-col.cb-nav-main.cb-bg-white > div");
+    if (!NODE) return;
+
+    NODE.style.color = "lightgray";
+  } catch (error) {
+    console.error(error);
+  }
+}
