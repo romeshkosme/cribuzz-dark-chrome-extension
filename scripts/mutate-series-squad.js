22e16c84e53dfd33d622ec661cd501acdfa99600
Romesh Kosme <romesh.30@gmail.com>
2024-05-09 18:57:00 +0530

    feat: series squad dark mode done

--- scripts/mutate-series-squad.js
+++ scripts/mutate-series-squad.js
@@ -9,8 +9,27 @@ export default function mutate_series_squad() {
     match_nav_links()
     mutate_match_count_length()
     mutate_container_bg()
+    mutate_player_text()
+    mutate_country_list()
+    subscribe();
   } catch (error) {
     console.error(error)
+  }
+}
+
+function subscribe() {
+  const TARGET = document.querySelector("#page-wrapper > div.cb-col-100.cb-col.cb-srs-sqd-box > div > div")
+  if (TARGET) {
+    const callback = (mutation, observer) => {
+      mutate_country_list()
+      mutate_player_text()
+    }
+
+    const observer = new MutationObserver(callback);
+
+    observer.observe(TARGET, { attributes: true, childList: true, subtree: true})
+
+    SUBSCRIPTION_LIST.push(observer);
   }
 }
 
@@ -18,10 +37,47 @@ function mutate_container_bg() {
   try {
     const NODE = document.querySelector("#page-wrapper > div.cb-col-100.cb-col.cb-srs-sqd-box > div")
     if (!NODE) return
-   console.log("NODE", NODE) 
+
     NODE.style.backgroundColor = CONTAINER_BG;
     NODE.style.color = "#fff";
   } catch (error) {
     console.error(error)
+  }
+}
+
+function mutate_player_text() {
+  try {
+    const NODE = document.getElementById("squadPlayers");
+    if (!NODE) return;
+
+    const LINKS = NODE.querySelectorAll("a");
+
+    for (const link of LINKS) {
+      link.style.color = "#fff"
+
+      const ROLE = link.querySelector(".cb-text-gray");
+      ROLE.style.color = "lightgray"
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+function mutate_country_list() {
+  try {
+    const NODE = document.querySelector("#page-wrapper > div.cb-col-100.cb-col.cb-srs-sqd-box > div > div > div.cb-col.cb-col-20")
+    if (!NODE) return
+
+    for (const link of NODE.children) {
+      link.style.color = "#fff";
+      if (link.classList.contains("cb-stats-lft-tab-active")) {
+        link.style.backgroundColor = DARK_BLOCK
+      } else {
+        link.style.backgroundColor = CONTAINER_BG
+      }
+    }
+  } catch (error) {
+    console.error(error);
+    
   }
 }
